81826cb7a8b00bccb37310abac947f718ed5f696
Bryce Hughson <64735781+Bryce-Cream@users.noreply.github.com>
2023-06-26 21:13:14 -0400

    big cup updates appropriately when going from full to 0

--- 16. Drink Water Application/script.js
+++ 16. Drink Water Application/script.js
@@ -3,6 +3,10 @@ const litres = document.getElementById('litres');
 const percentage = document.getElementById('percentage');
 const remaining = document.getElementById('remaining');
 
+
+updateBigCup();
+
+
 smallCups.forEach((cup, idx) => {
     cup.addEventListener('click', () => highlightCups(idx))
 });
@@ -21,4 +25,37 @@ function highlightCups(idx)
         else
             cup.classList.remove('full');
     })
+
+    updateBigCup();
+}
+
+function updateBigCup()
+{
+    //Get number of full cups
+    const fullCups = document.querySelectorAll('.cup-small.full').length;
+
+    const totalCups = smallCups.length;
+
+    if(fullCups === 0)
+    {
+        percentage.style.visibility = 'hidden';
+        percentage.style.height = 0;
+    }
+    else
+    {
+        percentage.style.visibility ='visible';
+        percentage.style.height = `${fullCups / totalCups * 330}px`;
+        percentage.innerText = `${fullCups/totalCups*100}%`;
+    }
+
+    if(fullCups === totalCups)
+    {
+        remaining.style.visibility = 'hidden';
+        remaining.style.height = 0;
+    }
+    else
+    {
+        remaining.style.visibility = 'visible';
+        litres.innerText = `${2-(250 * fullCups / 1000)}L`
+    }
 }
