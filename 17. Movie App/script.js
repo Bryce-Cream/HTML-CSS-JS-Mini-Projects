b211ee0334827fb6149e56264d8051a66d97d00d
Bryce Hughson <64735781+Bryce-Cream@users.noreply.github.com>
2023-07-11 00:48:33 -0400

    removed double movie element issue

--- 17. Movie App/script.js
+++ 17. Movie App/script.js
@@ -29,16 +29,14 @@ function showMovies(movies)
 
         movieEl.innerHTML = 
         `
-        <div class="movie">
-            <img src="${IMG_PATH + poster_path}" alt="${title}">
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-            </div>
-            <div class="overview">
-                <h3>Overview</h3>
-                ${overview}
-            </div>
+        <img src="${IMG_PATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview</h3>
+            ${overview}
         </div>
         `
         main.appendChild(movieEl);
